2bc5c759d61cd8255208befb092d9011aaa6310f
saturnn29 <125258844+saturnn29@users.noreply.github.com>
2024-03-23 19:42:46 +0700

    Delete src directory

--- src/Pages/Doctor/Contact/contact.js
+++
@@ -1,8 +0,0 @@
-import Counter from "../../Patient/Myfinancials/contact";
-import withAuth from "../../withAuth.js";
-
-function Contact() {
-    return <Counter/>;
-}
-
-export default withAuth(Contact, 'doctor');
